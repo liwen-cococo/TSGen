9be2cdff3e540a2fbfee07ef7ad3023e418b9f82
liwen-cococo <3407617286@qq.com>
2018-04-03 10:31:39 +0800

    version 1.0

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ if __name__ == '__main__':
     mo = MakeOutliers(points)
     points_1, anomaly_1 = mo.make_outliers()
     print 'done 1'
-    
+    """
     mb = MakeBehaviorAnomaly(points, cycle_len)
     points_2, anomaly_2 = mb.make_anomaly()
     print 'done 2'
-    
-    points, anomaly = joinAnomaly(points_1, anomaly_1, points_2, anomaly_2)
+    """
+    #points, anomaly = joinAnomaly(points_1, anomaly_1, points_2, anomaly_2)
     #print 'done all'
-    name = 'd3-22yw'
-    drawPng(points, anomaly, name+'.csv', name+'.json', name+'.png')
+    name = 'd4-22yw-outliers'
+    drawPng(points_1, anomaly_1, name+'.csv', name+'.json', name+'.png')
     
